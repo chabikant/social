f829ed468ddfa04b38f4174b43201261883f5a3f
himanshu7377 <him.singh7069@gmail.com>
2023-09-08 19:27:48 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import Header from './component/Header';
-import VideoCard from './component/VideoCard.js';
+import Header from '../src/component/Header';
+import VideoCard from '../src/component/VideoCard';
+import '../src/style.css';
 
-import './component/style.css';
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (searchQuery.trim() !== '') {
       fetch(`https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=${searchQuery}&numResults=10`)
-        .then(response => response.json())
-        .then(data => {setSearchResults(data)
-          console.log(data)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP Error! Status: ${response.status}`);
+          }
+          return response.json();
         })
-        .catch(error => console.error('Error fetching data:', error));
+        .then(data => {
+          console.log('API Response:', data); // Log the response to the console
+          setSearchResults(data.results);
+          setError(null); // Clear any previous errors
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+          setSearchResults([]); // Clear search results on error
+          setError(error.message); // Set the error message
+        });
     }
   }, [searchQuery]);
 
   return (
     <div className="App">
-    <Header onSearch={(query) => setSearchQuery(query)} />
-    <div className="results">
-      {Array.isArray(searchResults.results) && searchResults.results.length > 0 ? (
-        searchResults.results.map((video, index) => (
-          <VideoCard key={index} video={video} query={searchQuery} />
-        ))
-      ) : (
-        <p>No results found.</p>
-      )}
+      <Header onSearch={(query) => setSearchQuery(query)} />
+      <div className="results">
+        {error ? (
+          <p>Error: {error}</p>
+        ) : searchResults && searchResults.length > 0 ? (
+          searchResults.map((video, index) => (
+            <VideoCard key={index} video={video} />
+          ))
+        ) : (
+          <p>No results found.</p>
+        )}
+      </div>
     </div>
-  </div>
   );
 }
 
